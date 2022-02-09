06de2877771a62ef65e36ab0df7687239086d3d8
Sitoo <uo277915@gmail.com>
2022-02-09 13:06:31 +0100

    SDI-2122-409-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi/Sdi2122409SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122409SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122409SpringApplication.class, args);
+    }
+
+}
