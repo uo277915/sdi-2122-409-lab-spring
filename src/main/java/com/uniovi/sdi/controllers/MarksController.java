8bf241e3fee460ba6c7531aeea3f42c87cf093be
Sitoo <uo277915@gmail.com>
2022-02-22 22:23:06 +0100

    SDI-2122-409-3.1-Thymeleaf, fragmentos.

--- src/main/java/com/uniovi/sdi/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi/controllers/MarksController.java
@@ -57,4 +57,10 @@ public class MarksController {
         marksService.addMark(mark);
         return "redirect:/mark/details/" + id;
     }
+
+    @RequestMapping("/mark/list/update")
+    public String updateList(Model model) {
+        model.addAttribute("markList", marksService.getMarks());
+        return "mark/list :: tableMarks";
+    }
 }
